7881234d34a3bfca2390c4a873164febf9ea7c00
Carlosgadelha <josecarlos.gadelha10@gmail.com>
2021-07-26 08:02:34 -0300

    add Inline keyboards and on-the-fly updating

--- packages/bot_telegram/brothercast/bot.js
+++ packages/bot_telegram/brothercast/bot.js
@@ -1,10 +1,12 @@
 const Api = require('telegram-bot-api')
 const TG = require('telegram-bot-api')
+const  stringSimilarity  =  require ( "string-similarity" ) ;
 
 const fs = require('fs')
 
 const api = new TG({
-    token: '1922668081:AAFm8L9jONuifmn9v31rXNkLCcdKVekJS_U'
+    token: '1922668081:AAFm8L9jONuifmn9v31rXNkLCcdKVekJS_U',
+    polling: true
 })
 
 
@@ -22,7 +24,7 @@ api.start()
 })
 .catch(console.err)
 
-// Receive messages via event callback
+
 api.on('update', update => {
     const chat_id = update.message.chat.id
 
@@ -30,16 +32,73 @@ api.on('update', update => {
     // update object is defined at
     // https://core.telegram.org/bots/api#update
 
+    if(update.message.text == '/start'){
+      api.sendMessage({
+        chat_id: chat_id,
+
+        text: 'Vamos fazer uma aposta ? '
+    })
+    };
+
      //api.sendMessager(update.message.chat.id, 'jgjgh')
     //api.sendMessage(update.message.chat.id,`Olá ${update.message.chat.first_name}`)
+
+
     api.sendMessage({
         chat_id: chat_id,
 
-        text: 'Obrigado pela mensagem'
-    })
+        text: 'Obrigado pela mensagem',
 
-    console.log(update.message.chat.id)
+        reply_markup: {
+          inline_keyboard: [
+              [
+                  {
+                      text: 'test1',
+                      callback_data: 't1',
+                  }
+              ],
+              [
+                  {
+                      text: 'test2',
+                      callback_data: 't2',
+                  }
+              ],
+              [
+                  {
+                      text: 'next',
+                      callback_data: 'next',
+                  }
+              ]
+
+          ]
+      }
+
+
+  })
+
+//api.on('callback_query', query => {
+//  console.log(query)
+    //const chat_id = update.message.chat.id
+
+//})
+
+    var kkk = [
+      "ASIAN HASDICAP",
+      "BOTH MATCH",
+      "FINAL RESULT",
+      "DOUBLE HYPOTHESIS",
+    ];
+
+    var matches = stringSimilarity.findBestMatch(update.message.text,kkk.map(function (item)  {
+      return item;
+    }));
+    console.log(update)
+
+    //console.log(matches.bestMatch.target)
 })
+
+
+
 
 
 
