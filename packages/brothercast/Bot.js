2594af1751451f4084edbbb2657b27428c3d0625
Carlosgadelha <josecarlos.gadelha10@gmail.com>
2021-07-29 10:18:27 -0300

    correct monorepo structure implementation 1

--- packages/brothercast/Bot.js
+++ packages/brothercast/Bot.js
@@ -6,6 +6,131 @@ const bot = new TelegramBot(token, {
     polling: true
 });
 
+const units = [[
+  {
+      text: '0.25',
+      callback_data: JSON.stringify({
+          command: 'confirmar',
+      })
+  },
+  {
+      text: ' 0.5 ',
+      callback_data: JSON.stringify({
+          command: 'confirmar',
+
+      })
+  },
+  {
+      text: ' 1.0 ',
+      callback_data: JSON.stringify({
+          command: 'confirmar',
+
+      })
+  },
+  {
+      text: ' 2.0',
+      callback_data: JSON.stringify({
+          command: 'confirmar',
+
+      })
+  },
+  {
+      text: ' 3.0',
+      callback_data: JSON.stringify({
+          command: 'confirmar',
+
+      })
+  },
+  {
+      text: ' 4.0',
+      callback_data: JSON.stringify({
+          command: 'confirmar',
+
+      })
+  }]];
+
+const bet_type = [
+
+  [{
+      text: ' Asian Handicap ',
+      callback_data: JSON.stringify({
+         command: 'Asian',
+
+      })
+  }],
+  [{
+      text: ' Both mach ',
+      callback_data: JSON.stringify({
+         command: 'Both',
+
+      })
+  }],
+  [{
+      text: ' Final Result ',
+      callback_data: JSON.stringify({
+         command: 'Final',
+
+      })
+  }],
+  [{
+      text: ' Double Hypothesis ',
+      callback_data: JSON.stringify({
+        command: 'Double',
+
+      })
+  }]
+
+  ]
+
+const games = [
+
+  [{
+      text: ' A.Madrid (CarlWhizzer) vs Barcelona (Void) ',
+      callback_data: JSON.stringify({
+        command: 'bet_type',
+
+      })
+  }],
+  [{
+      text: 'Valencia (WeeMan) vs Sevilla (ViTo)',
+      callback_data: JSON.stringify({
+        command: 'bet_type',
+
+      })
+  }]
+
+  ]
+
+const both_etapa = [
+
+  [{
+      text: ' Yes ',
+      callback_data: JSON.stringify({
+         command: 'Both_opt',
+
+      })
+  }],
+  [{
+      text: ' No ',
+      callback_data: JSON.stringify({
+         command: 'Both_opt',
+
+      })
+  }]
+
+
+  ]
+
+const opts_gerais = [
+  ['bet_type','Escolha o tipo da aposta', bet_type],
+  ['Asian','Escolha a unidade', units],
+  ['Both','etapa',both_etapa],
+  ['Both_opt','Escolha a unidade',units]
+]
+
+
+
+
 bot.onText(/\/start/, (msg, match) => {
     console.log(msg);
     const opts = {
@@ -30,38 +155,7 @@ bot.onText(/\/start/, (msg, match) => {
 
 });
 
-bot.onText(/Apostar/i, (msg, match) => {
-    const opts = {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                        text: 'BTC',
-                        callback_data: JSON.stringify({
-                            command: 'price',
-                            'base': 'BTC'
-                        })
-                    },
-                    {
-                        text: 'USD',
-                        callback_data: JSON.stringify({
-                            command: 'price',
-                            'base': 'USD'
-                        })
-                    },
-                    {
-                        text: 'CNY',
-                        callback_data: JSON.stringify({
-                            command: '2',
-                            'base': 'CNY'
-                        })
-                    }
 
-                ]
-            ]
-        }
-    };
-    bot.sendMessage(msg.chat.id, 'Choose base currency', opts);
-});
 
 bot.on('callback_query', function onCallbackQuery(callbackQuery) {
     const data = JSON.parse(callbackQuery.data);
@@ -70,133 +164,46 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
         chat_id: msg.chat.id,
         message_id: msg.message_id,
     };
-    let text;
+
     if (data.command === 'fifa') {
 
-      const opts_bet = {
-          reply_to_message_id: msg.message_id,
-          reply_markup: JSON.stringify({
-            inline_keyboard: [[{
-              text: ' A.Madrid (CarlWhizzer) vs Barcelona (Void) ',
-              callback_data: JSON.stringify({
-                   command: 'price',
+        const opts_bet = {
+           reply_to_message_id: msg.message_id,
+           reply_markup: JSON.stringify({
+             inline_keyboard: games
+           })
+        };
 
-          }) }],
-          [{
-            text: 'Valencia (WeeMan) vs Sevilla (ViTo)',
-            callback_data: JSON.stringify({
-                 command: 'price',
+        if(msg.chat.type=="private"){
 
-        }) }]]
-        })
-    };
-    if(msg.chat.type=="private")
+           bot.sendMessage(msg.chat.id, `Escolha um dos jogos: `, opts_bet);
+           bot.answerCallbackQuery(callbackQuery.id);
+        }
 
-        bot.sendMessage(msg.chat.id, `Escolha um dos jogos: `, opts_bet);
-        bot.answerCallbackQuery(callbackQuery.id);
+    }
 
-    }else{
-      if (data.command === 'price') {
-        bot.sendMessage(opts.chat_id, `Escolha o tipo da aposta`,{
+    for (var i = 0; i < opts_gerais.length; i++) {
+
+
+      if (data.command === opts_gerais[i][0]) {
+        bot.sendMessage(opts.chat_id, `${opts_gerais[i][1]}`,{
+          //reply_to_message_id: msg.message_id,
           reply_markup: {
-              inline_keyboard: [[{
-                text: ' Asian Handicap ',
-                callback_data: JSON.stringify({
-                     command: 'Asian',
-                     'base': 'BTC'
-                }) }],[{
-                text: ' Both mach ',
-                callback_data: JSON.stringify({
-                     command: 'Both',
-                     'base': 'BTC'
-                }) }],[{
-                text: ' Final Result ',
-                callback_data: JSON.stringify({
-                    command: 'Final',
-                    'base': 'BTC'
-                }) }],[{
-                text: ' Double Hypothesis ',
-                callback_data: JSON.stringify({
-                    command: 'Double',
-                    'base': 'BTC'
-        }) }]],
+              inline_keyboard: opts_gerais[i][2],
           }
         });
         bot.answerCallbackQuery(callbackQuery.id);
-
-      }else{
-        if (data.command === 'Asian') {
-          bot.sendMessage(opts.chat_id, `Escolha a unidade`,{
-            reply_markup: {
-                inline_keyboard: [[{
-                  text: '0.25',
-                  callback_data: JSON.stringify({
-                       command: 'confirmar',
-                       'base': 'BTC'
-                  }) },{
-                  text: ' 0.5 ',
-                  callback_data: JSON.stringify({
-                       command: 'confirmar',
-                       'base': 'BTC'
-                  }) },{
-                  text: ' 1.0 ',
-                  callback_data: JSON.stringify({
-                      command: 'confirmar',
-                      'base': 'BTC'
-                  }) },{
-                  text: ' 2.0',
-                  callback_data: JSON.stringify({
-                      command: 'confirmar',
-                      'base': 'BTC'
-                  }) },{
-                  text: ' 3.0',
-                  callback_data: JSON.stringify({
-                      command: 'confirmar',
-                      'base': 'BTC'
-                  }) },{
-                  text: ' 4.0',
-                  callback_data: JSON.stringify({
-                      command: 'confirmar',
-                      'base': 'BTC'
-                  }) }]]
-            }
-          });
-          bot.answerCallbackQuery(callbackQuery.id);
-
-        }
 
       }
 
     }
 
+
 });
 
-bot.on('message', console.log);
+//bot.on('message', console.log);
 
-const indices = [];
-const y = ['a','b','c','d','e','f','g','h','i'];
 
-for (var i = 0; i < 20; i++) {
-
-  const newobject = [{
-    text: ' teste ',
-    callback_data: JSON.stringify({
-         command: 'price',
-         'base': 'BTC'
-}) }];
-
-  indices.push(newobject)
-
-};
-
-bot.onText(/Both mach/i, (msg, match) => {
-  const opts = {
-      reply_markup: {
-          inline_keyboard: indices
-      }
-  };
-  bot.sendMessage(msg.chat.id, 'Jogos disponiveis', opts);
-});
 
 
 
